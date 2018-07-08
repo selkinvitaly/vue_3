a374948cb225aecaef9238d49e4015ac4cd328fe
Selkin Vitaly <contact@selkinvitaly.ru>
2018-07-09 00:29:28 +0300

    minor review changes

--- src/components/TableWithUsers/PerPage.vue
+++ src/components/TableWithUsers/PerPage.vue
@@ -2,21 +2,10 @@
     <div>
         <ul id="dropdown2" class="dropdown-content">
             <li
-                :class="{'active': amount === 3}"
-                @click="amount = 3"
-            ><a>3 users</a></li>
-            <li
-                :class="{'active': amount === 5}"
-                @click="amount = 5"
-            ><a>5 users</a></li>
-            <li
-                :class="{'active': amount === 10}"
-                @click="amount = 10"
-            ><a>10 users</a></li>
-            <li
-                :class="{'active': amount === 20}"
-                @click="amount = 20"
-            ><a>20 users</a></li>
+                v-for="cnt in amountOptions"
+                :class="{'active': amount === cnt}"
+                @click="amount = cnt"
+            ><a>{{cnt}} users</a></li>
         </ul>
         <a ref="dropdown" class="btn dropdown-trigger" data-target="dropdown2">on page<i class="material-icons right">arrow_drop_down</i></a>
     </div>
@@ -24,6 +13,7 @@
 
 <script lang="ts">
 import Vue from 'vue';
+import * as isNumber from 'lodash/isNumber';
 import { Component, Model, Watch } from 'vue-property-decorator';
 import { Dropdown } from 'materialize-css';
 
@@ -39,6 +29,11 @@ export default class PerPage extends Vue {
     amount = this.perPage;
 
     dropdownInstance: Dropdown | null = null;
+
+    get amountOptions(): number[] {
+        return (Object as any).values(PageSizeTypes)
+            .filter(isNumber);
+    }
 
     @Watch('amount')
     changePerPage() {
