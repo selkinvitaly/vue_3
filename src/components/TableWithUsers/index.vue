bd5e81d9671c06d2e2a364944663c22287126c81
Selkin Vitaly <contact@selkinvitaly.ru>
2018-07-10 00:51:59 +0300

    refactor user-list

--- src/components/TableWithUsers/index.vue
+++ src/components/TableWithUsers/index.vue
@@ -1,72 +1,197 @@
 <template>
-    <div class="table-users">
-        <div class="header">
-            <slot name="header"></slot>
-        </div>
-        <table class="striped responsive-table">
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Avatar</th>
-                    <th scope="col">Delete</th>
-                </tr>
-            </thead>
+    <div>
+        <ProgressLoader
+            v-if="isLoading"
+        />
+        <Alert
+            v-if="isFailed"
+            :message="errorMessage"
+        />
+        <Alert
+            v-if="usersNotFound"
+            message="users not found!"
+        />
+        <DeleteUserModal
+            v-if="deleteModalIsVisible"
+            @confirm="confirmDeletionUser"
+            @cancel="markAsPendingDeletion(null)"
+        >
+            <h5 slot="header">Confirm user deletion!</h5>
+            <div slot="body">Are you sure that you want delete this user?</div>
+        </DeleteUserModal>
 
-            <tbody>
-                <tr
-                    v-for="(user, index) in users"
-                    is="UserItem"
-                    :index="getOrderByIndex(index)"
-                    :key="user.uuid"
-                    :user="user"
-                    @view-user="viewUser"
-                    @delete-user="markAsPendingDeletion"
-                ></tr>
-            </tbody>
-        </table>
-        <div class="footer">
-            <div class="footer__bars">
-                <slot name="footer"></slot>
+        <div class="table-users">
+            <div class="header">
+                
+                <div class="controls">
+                    <div class="per-page">
+                        <span class="per-page__label">total users: {{totalUsers}}</span>
+                        <PerPage
+                            v-model="pageSize"
+                        />
+                    </div>
+                </div>
+
+            </div>
+            <table class="striped responsive-table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Avatar</th>
+                        <th scope="col">Delete</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr
+                        v-for="(user, index) in users"
+                        is="UserItem"
+                        :index="getOrderByIndex(index)"
+                        :key="user.uuid"
+                        :user="user"
+                        @view-user="viewUser"
+                        @delete-user="markAsPendingDeletion"
+                    ></tr>
+                </tbody>
+            </table>
+            <div class="footer">
+                <div class="footer__bars">
+                    <PageNav
+                        v-model="pageNav"
+                        :size="pageSize"
+                        :total="totalUsers"
+                    />
+                </div>
             </div>
         </div>
+
     </div>
+    
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import { Component, Prop } from 'vue-property-decorator';
+import { Component, Watch } from 'vue-property-decorator';
 
 import UserItem from './UserItem.vue';
-import { User } from '../../models/users';
+import PageNav from './PageNav.vue';
+import PerPage from './PerPage.vue';
+import ProgressLoader from '../ProgressLoader.vue';
+import DeleteUserModal from '../DeleteUserModal.vue';
+import Alert from '../Alert.vue';
+import { User, LoadingStatus } from '../../models/users';
+import { PageSizeTypes } from '../../models/pagination';
+import { getUsers } from '../../services/get-users';
+import { deleteUserById } from '../../services/delete-user.by-id';
 
 
 @Component({
     components: {
-        UserItem
-    },
+        UserItem, PageNav, PerPage, ProgressLoader, DeleteUserModal, Alert
+    }
 })
 export default class TableWithUsers extends Vue {
 
-    @Prop()
-    users!: User[];
+    users: User[] = [];
+    totalUsers = 0;
+    loadingStatus = LoadingStatus.Initial;
+    errorMessage: string | null = null;
+    pageNav = 1;
+    pageSize = PageSizeTypes.ThreeUsersPerPage;
+    userIdPendingDeletion: string | null = null;
 
-    @Prop()
-    pageNav!: number;
 
-    @Prop()
-    pageSize!: number;
+    get isLoading(): boolean {
+        return this.loadingStatus === LoadingStatus.Loading;
+    }
+
+    get isFailed(): boolean {
+        return this.loadingStatus === LoadingStatus.Failed;
+    }
+
+    get isSuccess(): boolean {
+        return this.loadingStatus === LoadingStatus.Success;
+    }
+
+    get usersNotFound(): boolean {
+        return this.isSuccess && !this.users.length;
+    }
+
+    get deleteModalIsVisible(): boolean {
+        return !!this.userIdPendingDeletion;
+    }
+
+    get skip(): number {
+        return (this.pageNav - 1) * this.pageSize;
+    }
+
+    get limit(): number {
+        return this.pageSize;
+    }
+
+    get maxAvailablePage(): number {
+        return Math.ceil(this.totalUsers / this.pageSize);
+    }
+
+
+    mounted() {
+        this.fetchUsers();
+    }
+
 
     getOrderByIndex(i: number): number {
         return (this.pageNav - 1) * this.pageSize + i;
     }
 
     viewUser(userId: string): void {
-        this.$emit('view-user', userId);
+        this.$router.push({
+            name: 'user-details',
+            params: {
+                id: userId
+            }
+        });
     }
 
     markAsPendingDeletion(userId: string | null): void {
-        this.$emit('delete-user', userId);
+        this.userIdPendingDeletion = userId;
+    }
+
+    @Watch('pageNav')
+    @Watch('pageSize')
+    updateUserList(): void {
+        if (this.pageNav > this.maxAvailablePage) {
+            this.pageNav = 1;
+        }
+        this.fetchUsers();
+    }
+
+    fetchUsers(): void {
+        this.loadingStatus = LoadingStatus.Loading;
+        getUsers({ skip: this.skip, limit: this.limit })
+            .then(({ total, users }) => {
+                this.loadingStatus = LoadingStatus.Success;
+                this.users = users;
+                this.totalUsers = total;
+            })
+            .catch((err) => {
+                this.loadingStatus = LoadingStatus.Failed;
+                this.errorMessage = err.message;
+            });
+    }
+
+    confirmDeletionUser(): void {
+        this.loadingStatus = LoadingStatus.Loading;
+        deleteUserById(this.userIdPendingDeletion!)
+            .then(() => {
+                this.loadingStatus = LoadingStatus.Success;
+                this.users = this.users.filter(u => u.uuid !== this.userIdPendingDeletion);
+                this.userIdPendingDeletion = null;
+            })
+            .catch(err => {
+                this.loadingStatus = LoadingStatus.Failed;
+                this.errorMessage = err.message;
+            });
     }
 
 }
@@ -75,10 +200,24 @@ export default class TableWithUsers extends Vue {
 
 <style lang="stylus" scoped>
     .footer {
-        text-align: center
+        text-align center
 
         &__bars {
-            display: inline-block
+            display inline-block
+        }
+    }
+
+    .controls {
+        padding 10px 5px 0
+    }
+
+    .per-page {
+        display flex
+
+        &__label {
+            font-weight bold
+            line-height 36px
+            margin-right 10px
         }
     }
 </style>
--- src/views/UserList.vue
+++ src/views/UserList.vue
@@ -1,188 +1,20 @@
 <template>
     <div>
-        <ProgressLoader
-            v-if="isLoading"
-        />
-        <Alert
-            v-if="isFailed"
-            :message="errorMessage"
-        />
-        <Alert
-            v-if="usersNotFound"
-            message="users not found!"
-        />
-        <DeleteUserModal
-            v-if="deleteModalIsVisible"
-            @confirm="confirmDeletionUser"
-            @cancel="markAsPendingDeletion(null)"
-        >
-            <h5 slot="header">Confirm user deletion!</h5>
-            <div slot="body">Are you sure that you want delete this user?</div>
-        </DeleteUserModal>
-        
-        <TableWithUsers
-            v-if="isSuccess"
-            :users="users"
-            :page-nav="pageNav"
-            :page-size="pageSize"
-            @view-user="viewUser"
-            @delete-user="markAsPendingDeletion"
-        >
-            <div class="controls" slot="header">
-                <div class="per-page">
-                    <span class="per-page__label">total users: {{totalUsers}}</span>
-                    <PerPage
-                        v-model="pageSize"
-                    />
-                </div>
-            </div>
-
-            <div slot="footer">
-                <PageNav
-                    v-model="pageNav"
-                    :size="pageSize"
-                    :total="totalUsers"
-                />
-            </div>
-        </TableWithUsers>
+        <TableWithUsers />
     </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import { Component, Watch } from 'vue-property-decorator';
+import { Component } from 'vue-property-decorator';
 
 import TableWithUsers from '../components/TableWithUsers/index.vue';
-import PageNav from '../components/TableWithUsers/PageNav.vue';
-import PerPage from '../components/TableWithUsers/PerPage.vue';
-import { LoadingStatus, User } from '../models/users';
-import { PageSizeTypes } from '../models/pagination';
-import { getUsers } from '../services/get-users';
-import { deleteUserById } from '../services/delete-user.by-id';
-import Alert from '../components/Alert.vue';
-import ProgressLoader from '../components/ProgressLoader.vue';
-import DeleteUserModal from '../components/DeleteUserModal.vue';
 
 
 @Component({
     components: {
-        ProgressLoader, Alert, TableWithUsers, DeleteUserModal,
-        PageNav, PerPage
-    },
+        TableWithUsers
+    }
 })
-export default class UserList extends Vue {
-
-    users: User[] = [];
-    totalUsers: number = 0;
-    loadingStatus = LoadingStatus.Initial;
-    errorMessage: string | null = null;
-    pageSize = PageSizeTypes.ThreeUsersPerPage;
-    pageNav = 1;
-
-    userIdPendingDeletion: string | null = null;
-
-    get isLoading(): boolean {
-        return this.loadingStatus === LoadingStatus.Loading;
-    }
-
-    get isFailed(): boolean {
-        return this.loadingStatus === LoadingStatus.Failed;
-    }
-
-    get isSuccess(): boolean {
-        return this.loadingStatus === LoadingStatus.Success;
-    }
-
-    get usersNotFound(): boolean {
-        return this.isSuccess && !this.users.length;
-    }
-
-    get deleteModalIsVisible(): boolean {
-        return !!this.userIdPendingDeletion;
-    }
-
-    get skip(): number {
-        return (this.pageNav - 1) * this.pageSize;
-    }
-
-    get limit(): number {
-        return this.pageSize;
-    }
-
-    get maxAvailablePage(): number {
-        return Math.ceil(this.totalUsers / this.pageSize);
-    }
-
-    mounted() {
-        this.fetchUsers();
-    }
-
-    @Watch('pageNav')
-    @Watch('pageSize')
-    updateUserList(): void {
-        if (this.pageNav > this.maxAvailablePage) {
-            this.pageNav = 1;
-        }
-        this.fetchUsers();
-    }
-
-    fetchUsers(): void {
-        this.loadingStatus = LoadingStatus.Loading;
-        getUsers({ skip: this.skip, limit: this.limit })
-            .then(({ total, users }) => {
-                this.loadingStatus = LoadingStatus.Success;
-                this.users = users;
-                this.totalUsers = total;
-            })
-            .catch((err) => {
-                this.loadingStatus = LoadingStatus.Failed;
-                this.errorMessage = err.message;
-            });
-    }
-
-    confirmDeletionUser(): void {
-        this.loadingStatus = LoadingStatus.Loading;
-        deleteUserById(this.userIdPendingDeletion!)
-            .then(() => {
-                this.loadingStatus = LoadingStatus.Success;
-                this.users = this.users.filter(u => u.uuid !== this.userIdPendingDeletion);
-                this.userIdPendingDeletion = null;
-            })
-            .catch(err => {
-                this.loadingStatus = LoadingStatus.Failed;
-                this.errorMessage = err.message;
-            });
-    }
-
-    viewUser(userId: string): void {
-        this.$router.push({
-            name: 'user-details',
-            params: {
-                id: userId
-            }
-        });
-    }
-
-    markAsPendingDeletion(userId: string | null): void {
-        this.userIdPendingDeletion = userId;
-    }
-
-}
+export default class UserList extends Vue {}
 </script>
-
-<style lang="stylus" scoped>
-    .controls {
-        padding: 10px 5px 0
-    }
-
-    .per-page {
-        display: flex
-
-        &__label {
-            font-weight: bold
-            line-height: 36px
-            margin-right: 10px
-        }
-    }
-
-</style>
